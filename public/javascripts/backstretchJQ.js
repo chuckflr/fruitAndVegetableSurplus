8e3024148e6a13e46b886e238a370545a51d728a
Carlos Flores <Carlos Flores>
2016-12-15 08:43:41 -0800

    updates

--- public/javascripts/backstretchJQ.js
+++
@@ -1,8 +0,0 @@
-$("#precontent").backstretch([
-  "/images/backstretch1.jpg",
-  "/images/backstretch2.jpg",
-  "/images/backstretch3.jpg"
-  ], {
-    fade: 750,
-    duration: 3000
-});
--- routes/index.js
+++ routes/index.js
@@ -14,8 +14,9 @@ router.get('/farmscoalinga', function(req, res, next) {
   res.render('farmscoalinga', { title: 'Farms Coalinga' });
 });
 
-router.get('/farmslemoore', function(req, res, next) {
-  res.render('farmslemoore', { title: 'Farms Lemoore' });
+router.get('/rancho', function(req, res, next) {
+  res.render('rancho', { title: 'Rancho Notso Grande' });
 });
+
 
 module.exports = router;
